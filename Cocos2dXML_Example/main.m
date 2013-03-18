2a7d018847127eb1061ed058df5b4b46a9ac53bf
Brad Bambara <brad.bambara@gmail.com>
2013-03-18 09:17:20 -0400

    Initial project setup

---
+++ Cocos2dXML_Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cocos2dXML_Example
+//
+//  Created by Brad Bambara on 3/18/13.
+//  Copyright (c) 2013 Robox Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
